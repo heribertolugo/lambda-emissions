98a154f365b7a8af34953c3f7377c6bcd4bd2846
Heriberto Lugo <heribertolugo@live.com>
2023-11-02 22:57:09 -0400

    added some unit tests. refactored code.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,6 +12,12 @@ import { SafeHtmlPipe } from './shared/pipes/SafeHtmlPipe';
 import { FormsModule } from '@angular/forms';
 import { HelpComponent } from './help/help.component';
 import { Note } from './shared/models/note';
+import { Group } from './shared/enums/groups';
+import { GroupData } from './shared/models/groupData';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { LambdaValues } from './shared/models/lambdaValues';
+import { ILambdaValues } from './shared/models/IlambdaValues';
 
 describe('AppComponent', () => {
   let appFixture: ComponentFixture<AppComponent>;
@@ -34,16 +40,17 @@ describe('AppComponent', () => {
         SafeHtmlPipe,
         HelpComponent
       ]
-    });
+    }).compileComponents();
 
     appFixture = TestBed.createComponent(AppComponent);
     appInstance = appFixture.componentInstance;
     appCompiled = appFixture.nativeElement as HTMLElement;
-  }
-  );
+  });
+
+  // #region Unit tests
 
   it('should create the app', () => {
-    expect(appInstance).toBeTruthy();
+    expect(appInstance).toBeDefined();
   });
 
   it(`should have as title 'Lambda Diagnostics'`, () => {
@@ -52,13 +59,12 @@ describe('AppComponent', () => {
 
   it('should toggle render NoteComponent. Initial is non-rendered', () => {
     appFixture.detectChanges();
-    expect(appCompiled.querySelector('app-note')).toBeNull(); // ensure it inits non-rendered
+    expect(appCompiled.querySelector('app-note'))
+      .withContext('note was not hidden by default').toBeNull(); // ensure it inits non-rendered
     appInstance.showNote(); // call toggle to render
-    appFixture.detectChanges();
     expect(appInstance.openNote).toBeTrue(); // ensure render flag is true
-    expect(appCompiled.querySelector('app-note')).toBeTruthy(); // ensure rendered
+    expect(appCompiled.querySelector('app-note')).toBeDefined(); // ensure rendered
     appInstance.setNote(new Note()); // call toggle to non-render
-    appFixture.detectChanges();
     expect(appInstance.openNote).toBeFalse(); // ensure render flag is false
     expect(appCompiled.querySelector('app-note')).toBeNull(); // ensure not rendered
   });
@@ -67,52 +73,87 @@ describe('AppComponent', () => {
     appFixture.detectChanges();
     expect(appCompiled.querySelector('app-help')).toBeNull(); // ensure it inits non-rendered
     appInstance.showHelp(); // call toggle to render
-    appFixture.detectChanges();
     expect(appInstance.openHelp).toBeTrue(); // ensure render flag is true
-    expect(appCompiled.querySelector('app-help')).toBeTruthy(); // ensure rendered
+    expect(appCompiled.querySelector('app-help')).toBeDefined(); // ensure rendered
     appInstance.closeHelp(); // call toggle to non-render
-    appFixture.detectChanges();
     expect(appInstance.openHelp).toBeFalse(); // ensure render flag is false
     expect(appCompiled.querySelector('app-help')).toBeNull(); // ensure not rendered
   });
 
   it('should render container div', () => {
-    appFixture.detectChanges();
-    expect(appCompiled.querySelector('.page_container')).toBeTruthy();
+    expect(appCompiled.querySelector('.page_container')).toBeDefined();
   });
 
   it('should render HeaderComponent', () => {
-    appFixture.detectChanges();
-    expect(appCompiled.querySelector('.page_container header app-header')).toBeTruthy();
+    expect(appCompiled.querySelector('.page_container header app-header')).toBeDefined();
   });
 
   it('should render LambdaCalculatorComponent', () => {
-    appFixture.detectChanges();
-    expect(appCompiled.querySelector('.page_container main app-lambdacalculator')).toBeTruthy();
+    expect(appCompiled.querySelector('.page_container main app-lambdacalculator')).toBeDefined();
   });
 
   it('should render BottomnavComponent', () => {
-    appFixture.detectChanges();
-    expect(appCompiled.querySelector('.page_container footer app-bottomnav')).toBeTruthy();
+    expect(appCompiled.querySelector('.page_container footer app-bottomnav')).toBeDefined();
   });
 
   it('should set note and groupName for NoteComponent using current active Group', () => {
-    /** @TODO loadNoteIntoComponent(noteComponent: NoteComponent) */
+    appInstance.showNote();
+    appFixture.detectChanges();
+    let noteDebug: DebugElement = appFixture.debugElement.query(By.directive(NoteComponent));
+    //let noteFixture: ComponentFixture<NoteComponent> = TestBed.createComponent(NoteComponent);
+    let noteInstance: NoteComponent = noteDebug.componentInstance; //noteFixture.componentInstance;
+    let activeGroup: Group = appInstance.getComparisonGroup();
+    let groupData: GroupData | undefined = appInstance['comparisons'].get(activeGroup); 
+    expect(groupData).toBeDefined();
+    let note: Note = groupData!.note;
+    appInstance.loadNoteIntoComponent(noteInstance);
+    expect(noteInstance.groupName).toEqual(activeGroup);
+    expect(noteInstance.note).toEqual(note);
   });
 
-  it('should set or create current active group and group data', () => {
-    /** @TODO setComparisonGroup(val: Group) */
+  it('should set current active group', () => {
+    for (const group of Object.values(Group)) {
+      appInstance.setComparisonGroup(group);
+      expect(appInstance.getComparisonGroup()).toEqual(group);
+    }    
   });
 
   it('should set group logo using group provided', () => {
-    /** @TODO updateGroupLogo(val: Group) */
+    let headerDebug: DebugElement = appFixture.debugElement.query(By.directive(HeaderComponent));
+    expect(headerDebug).withContext('headerDebug isnt truthy').toBeDefined();
+    let sectionLogoDebug: DebugElement = headerDebug.query(By.directive(SectionlogoComponent));
+    expect(sectionLogoDebug).withContext('sectionLogoDebug isnt truthy').toBeDefined();
+    let activeLogo: string = sectionLogoDebug.componentInstance.logoUrl;
+    expect(activeLogo).withContext('activeLogo isnt truthy').toBeDefined();
+    const initialGroup: Group = appInstance.getComparisonGroup();
+    expect(initialGroup).withContext('initialGroup isnt truthy').toBeDefined();
+    appFixture.detectChanges();
+    for (const group of Object.values(Group).filter(g => g != initialGroup)) {
+      appInstance['updateGroupLogo'](group);
+      expect(sectionLogoDebug.componentInstance.logoUrl).withContext('logo url did not change').not.toEqual(activeLogo);
+      activeLogo = sectionLogoDebug.componentInstance.logoUrl;
+    }
   });
 
   it('should set lambda values for the current group', () => {
-    /** @TODO updateLambdaValues(val: ILambdaValues) */
+    const testValues: ILambdaValues = new LambdaValues(1,2,3,4,5,6,7);
+    const testEqual: string = JSON.stringify(testValues);
+    
+    for (const group of Object.values(Group)) {
+      if (!appInstance['comparisons'].has(group))
+        appInstance['comparisons'].set(group, new GroupData(group, new LambdaValues()));
+        appInstance['activeGroup'] = group;
+      appInstance.updateLambdaValues(testValues);
+      expect(JSON.stringify(appInstance['comparisons']
+        .get(group)?.lambdaValues)).withContext(`${group} was not equal`)
+        .toEqual(testEqual);
+    }
   });
 
   it('should set calculator component values to the values in provided group', () => {
     /** @TODO updateCalculator(val: Group) */
   });
+  // #endregion
+
+  
 });
